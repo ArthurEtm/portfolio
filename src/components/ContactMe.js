4b9dc1b0da6e2b779ddd937abff2bdc2d5b04f64
Arthur Etmekdjian <AEtmekdjian7@gmail.com>
2020-02-12 00:22:38 -0500

    contact

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,39 +1,89 @@
-import React from "react";
-import emailjs from "emailjs-com";
-
-// import "./ContactMe.css";
-
-export default function ContactMe() {
-  function sendEmail(e) {
+import React, { Component } from "react";
+import * as emailjs from "emailjs-com";
+export default class ContactMe extends Component {
+  state = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+    subject: "",
+    message: ""
+  };
+  handleSubmit(e) {
     e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "gmail",
-        "template_FItNXuhV",
-        e.target,
-        "user_zkrkKUslJ8c2jKFHucWXR"
-      )
-      .then(
-        result => {
-          console.log(result.text);
-        },
-        error => {
-          console.log(error.text);
-        }
-      );
+    const { name, email, phoneNumber, message } = this.state;
+    let templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Air Stair Solutions",
+      phoneNumber: phoneNumber,
+      message_html: message
+    };
+    emailjs.send(
+      "gmail",
+      "template_VFZZ9dUH",
+      templateParams,
+      "user_XmSvfoBKQhPmLjMbCFfDq"
+    );
+    this.resetForm();
   }
-
-  return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+  resetForm() {
+    this.setState({
+      name: "",
+      email: "",
+      phoneNumber: "",
+      subject: "",
+      message: ""
+    });
+  }
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value });
+  };
+  render() {
+    return (
+      <div className="feedbackform">
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <div>
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange.bind(this, "name")}
+              placeholder="Name"
+            ></input>
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange.bind(this, "email")}
+              placeholder="Email"
+            />
+            <input
+              type="number"
+              name="phoneNumber"
+              value={this.state.phoneNumber}
+              onChange={this.handleChange.bind(this, "phoneNumber")}
+              placeholder="Phone"
+            />
+            <input
+              type="text"
+              name="subject"
+              value={this.state.subject}
+              onChange={this.handleChange.bind(this, "subject")}
+              placeholder="Subject"
+            />
+            <textarea
+              name="message"
+              rows="10"
+              value={this.state.message}
+              onChange={this.handleChange.bind(this, "message")}
+              placeholder="Message"
+            ></textarea>
+          </div>
+          <div>
+            <button type="submit">SEND</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
