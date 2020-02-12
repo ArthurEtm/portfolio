6c0942bcef4b15935abec831cfc07796712a27c8
Arthur Etmekdjian <AEtmekdjian7@gmail.com>
2020-02-11 19:44:10 -0500

    Updates

---
+++ precache-manifest.44d55bf8e9a80b21250b965999269217.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6331f83d6f08da8923d3e87231f0dd0e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "07bf0ca0ccc6c7827a53",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c6a0ec8d2fc2e1157bdd",
+    "url": "/portfolio/static/css/main.014fe582.chunk.css"
+  },
+  {
+    "revision": "07bf0ca0ccc6c7827a53",
+    "url": "/portfolio/static/js/2.bb4b0a2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/portfolio/static/js/2.bb4b0a2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a0ec8d2fc2e1157bdd",
+    "url": "/portfolio/static/js/main.a5167479.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
